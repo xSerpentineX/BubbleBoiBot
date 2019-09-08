877d69ef1f8d928c7a5ce5e8dc9e58fcd6d388fc
PotassiumSnek#6853 <54080082+TheLoveableBananaNoodle@users.noreply.github.com>
2019-09-08 22:07:38 +0100

    Update loopAll.bat

--- loopAll.bat
+++ loopAll.bat
@@ -1,10 +1,10 @@
 @mode 70,40
 @echo off
-title Bubbly Boi Bot v1.62 - Starter
+title Bubbly Boi Bot v1.63 - Starter
 
 echo.
 
-echo				 BubblyBoiBot v1.62
+echo				 BubblyBoiBot v1.63
 echo                 Original bot programmed by alekxeyuk
 echo     Spam Integrated Bot programmed by TheLoveableBubblyNoodle#6853
 echo         Batch support programmed by ! [( TheGamerX )]#7912
