0e5c0020447a7d639f65c527dbceb514075416d3
Catterall#6723 <54080082+TheLoveableBananaNoodle@users.noreply.github.com>
2019-10-06 00:02:48 +0100

    Update loop5001.bat

--- exec/loop5001.bat
+++ exec/loop5001.bat
@@ -1,11 +1,11 @@
 @echo off
-title Bubbly Boi Bot - Port 5001
+title BubbleBoiBot - Port 5001
 for %%I in (.) do set CurrDirName=%%~nxI
 if "%CurrDirName%"=="exec" (
 	cd..
 )
 :loop
-BubblyBoiBot.py 5001
+BubbleBoiBot.py 5001
 echo.
 echo ----------------------------
 echo.
